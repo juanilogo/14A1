513b5f65ebd0a5a2d01b2725811ac38d206060e4
Juan Lopez <lopez@jlautomation.de>
2025-07-18 21:05:07 +0200

    Actualizacion final00

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
-# Buscar el ejecutable llamado "main" (que tenga permisos de ejecución)
-MAIN_PATH=$(find . -type f -name main -executable | head -n 1)
-
-if [ -z "$MAIN_PATH" ]; then
-    echo "Error: ejecutable 'main' no encontrado."
-    exit 1
-fi
-
-"$MAIN_PATH" > output.txt
-
+../main > output.txt
 if grep -q "Hello, Embedded Linux World!" output.txt; then
     echo "Test passed."
     exit 0
